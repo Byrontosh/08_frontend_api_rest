5f2f70e9f53c8efd29f24dd435b59a6f9733430f
BYRON LOARTE <by_tosh20@hotmail.com>
2022-08-30 11:12:04 -0500

    Update App.js

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -3,33 +3,43 @@ import './App.css';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-export const App = () => {
-  const { logout } = useContext(AuthContext);
+
+export const App = () => 
+{
   const navigate = useNavigate();
 
-  const onLogout = () => {
-    navigate('/login', { replace: true })
-    logout()
-  };
+  const { user, logout } = useContext(AuthContext);
+  const tokenUser = localStorage.getItem('token')
+
+  const config = {
+    headers: { Authorization: `${tokenUser}` }
+  }; 
+  const onLogout = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post(
+            'http://localhost:8000/api/v1/logout',
+            { headers: { 'accept': 'application/json' } },
+            config
+        )
+        logout()
+        navigate('/login')       
+    } catch (error) {
+        console.log(error.response.data.message, 'error');
+    }
+}
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Prison System Portal</h1>
+        <h1>Prison System</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" onClick={onLogout}>Logout</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido - {user.username}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{backgroundColor:"red", padding:"5px", borderRadius:"10px"}} onClick={onLogout}>Logout</button>
       </header>
     </div>
   );
